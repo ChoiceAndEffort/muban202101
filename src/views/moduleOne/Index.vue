3584814aa39f7312c45d5b66c1193cf1aee13ddb
gonglei <gonglei@wsecar.com>
2021-01-16 18:29:08 +0800

    处理数据

--- src/views/moduleOne/Index.vue
+++ src/views/moduleOne/Index.vue
@@ -4,19 +4,35 @@
     <ul>
       <li v-for="item in list" :key="item.age">
         {{ item.name }}
+        <!-- <input type="text" v-model.trim="item.name" /> -->
       </li>
     </ul>
+    <div style="display:flex;">
+      <div class="oprate-btn" @click="handlerOpreate">更改list</div>
+      <div class="oprate-btn" @click="handlerOReset">重置list</div>
+    </div>
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
-
+const newList = [
+  {
+    name: "张安",
+    age: 16
+  },
+  {
+    name: "丽岛",
+    age: 18
+  }
+];
 export default {
   data() {
-    return {};
+    return {
+      newList: JSON.parse(JSON.stringify(newList))
+    };
   },
   computed: {
-    ...mapGetters("moduleOneStore", ["list"])
+    ...mapGetters("moduleOneStore", ["list", "item"])
   },
   methods: {
     getList() {
@@ -24,6 +40,12 @@ export default {
         name: "888888888",
         age: 123
       });
+    },
+    handlerOpreate() {
+      this.$store.commit("moduleOneStore/LIST", this.newList);
+    },
+    handlerOReset() {
+      this.getList();
     }
   },
   created() {
@@ -33,6 +55,14 @@ export default {
 </script>
 <style lang="scss" scoped>
 .module-one {
-  flex: 1;
+  .oprate-btn {
+    width: 100px;
+    padding: 7px 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-align: center;
+    cursor: pointer;
+    margin-right: 100px;
+  }
 }
 </style>
