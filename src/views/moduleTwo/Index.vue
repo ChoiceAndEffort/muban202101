081c6cffae6982f0bcd5809997bc7ef0eb04202a
gonglei <gonglei@wsecar.com>
2021-01-19 14:04:38 +0800

    update: 增加jsx语法

--- src/views/moduleTwo/Index.vue
+++ src/views/moduleTwo/Index.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="module-two"></div>
+  <div class="module-two">
+    <btn-operate />
+  </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
+import BtnOperate from "./btnOperate"; //方法一
 export default {
   data() {
     return {};
+  },
+  components: {
+    BtnOperate //方法一导入
   },
   computed: {
     ...mapGetters("moduleFourStore", ["list"])
   }
 };
 </script>
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.module-two {
+  .btn {
+    width: 100px;
+    color: blue;
+    padding: 10px 20px;
+    border: 1px solid #ccc;
+    text-align: center;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/moduleTwo/btnOperate.js
@@ -0,0 +1,26 @@
+//利用jsx语法写组件
+export default {
+  name: "btn-operate",
+  props: {
+    btnName: {
+      type: String,
+      required: false,
+      default: () => "新增"
+    }
+  },
+  render() {
+    return (
+      <div onClick={this.handlerAddClick} class="btn">
+        {this.btnName}
+      </div>
+    );
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handlerAddClick() {
+      console.log("增加");
+    }
+  }
+};
